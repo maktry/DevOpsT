8c275a0f6dd6c78304f80e1d8ad23664db39f488
Jamal Almaktari <maktry@gmail.com>
2020-09-17 15:28:06 +1000

    r

--- devOps/devOps/Controllers/WeatherForecastController.cs
+++ devOps/devOps/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace devOps.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cooooooooool", "Mild", "Warm", "Balmy", "Hooooooooooooot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -27,10 +27,11 @@ namespace devOps.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
+            int x = 0;
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                TemperatureC = rng.Next(-20, 20),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
